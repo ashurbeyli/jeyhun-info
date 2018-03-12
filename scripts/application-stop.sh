3b5821b662d6048370a6572e92a0c6d76a5dd22d
Ceyhun <c.ashurbeyli@gmail.com>
2018-03-13 03:19:50 +0400

    modified deployment scripts

---
+++ scripts/application-stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/jeyhun.info
+pm2 stop appProd || true
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+
+# update instance
+yum -y update
+
+# install node and npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+# install yarn
 npm install yarn -g
+
+# install pm2 module globally
+npm install -g pm2
+pm2 update
